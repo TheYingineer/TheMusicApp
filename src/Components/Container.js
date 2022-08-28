d855993b4ee929100531a16bd90468cb9e0659d3
TheYingineer <ying.yan.huang87@gmail.com>
2022-08-28 14:45:13 -0500

    added feature for logout

--- src/Components/Container.js
+++ src/Components/Container.js
@@ -6,7 +6,7 @@ import Paper from '@mui/material/Paper';
 
 export default function SimpleContainer(props) {
     return <Container maxWidth="xs">
-        <Paper sx={{paddingLeft: 5, paddingRight: 5, width: 300, height: 200}}>
+        <Paper sx={{paddingLeft: 5, paddingRight: 5, width: 300, height: 300}}>
             {props.children}
         </Paper>
     </Container>;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,7 +7,12 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
-export default function ButtonAppBar() {
+export default function ButtonAppBar({ state, setState }) {
+    function handleLogout() {
+        console.log('logout')
+        setState(!state)
+    }
+
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
@@ -24,7 +29,16 @@ export default function ButtonAppBar() {
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         Music App
                     </Typography>
-                    <Button color="inherit">Login</Button>
+                    {
+                        state ? (
+                            <Button onClick={handleLogout} color="inherit">Logout</Button>
+
+                        ) : (
+                            <Button color="inherit">Login</Button>
+
+                        )
+                    }
+
                 </Toolbar>
             </AppBar>
         </Box>
